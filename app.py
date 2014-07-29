5944a77afc178979cffcb2526daeb1f0d4b96010
Rob Ottaway <robottaway@gmail.com>
2014-07-29 02:59:26 -0400

    Work to modularize app

--- app.py
+++ app.py
@@ -2,12 +2,14 @@
 
 import subprocess  
 import os.path
+from os import listdir
 import uuid
 import sys
 import shutil
 import logging
 import json
-from flask import Flask
+
+from flask import Flask, jsonify
 from flask import request, Response
 from flask import abort, redirect, url_for
 from voluptuous import Schema, Required, All, Length, Range
@@ -76,8 +78,7 @@ def close_handlers(log):
 
 
 def run_and_log(args, build_log, cwd=None):
-    """Run given args as process, log output to given build logger.
-    """
+    """Run given args as process, log output to given build logger."""
     p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cwd)
     for line in p.stdout:
         yield line
@@ -89,8 +90,7 @@ def run_and_log(args, build_log, cwd=None):
 
 
 def get_short_hash(build, build_log):
-    """Get the short hash for the HEAD of the git repo.
-    """
+    """Get the short hash for the HEAD of the git repo."""
     args = ['git', 'rev-parse', '--short', 'HEAD']
     p = subprocess.Popen(args, stdout=subprocess.PIPE, cwd=build.location)
     p.wait()
@@ -102,8 +102,7 @@ def get_short_hash(build, build_log):
 
 
 def do_build(build):
-    """Takes given build and kicks it off
-    """
+    """Takes given build and kicks it off"""
     build_log = get_logger(build)
     try:
         for line in run_and_log(['git', 'clone', build.repo, build.location], build_log):
@@ -127,13 +126,17 @@ def do_build(build):
 
 
 class ApiError(Exception):
+    """Returned when failure of API.
+
+    Defaults to 400.
+    """
     code = 400
 
     def __init__(self, description, code=None, payload=None):
         Exception.__init__(self)
         self.description = description
-        if status_code is not None:
-            self.code = status_code
+        if code is not None:
+            self.code = code
 
     def to_dict(self):
         return { 'message': self.description, 'status_code': self.status_code }
@@ -146,6 +149,11 @@ app = Flask(__name__)
 app.debug = True
 
 
+@app.errorhandler(500)
+def error_ise(error):
+    return '{"status_code":500,"description":"oh noes!"}', 500
+
+
 @app.errorhandler(404)
 @app.errorhandler(400)
 @app.errorhandler(405)
@@ -155,8 +163,7 @@ def error_json(error):
 
 
 def post_build(name):
-    """Create or update a build
-    """
+    """Create or update a build."""
     try:
         build_schema(request.json)
     except MultipleInvalid as e:
@@ -170,14 +177,19 @@ def post_build(name):
     return "build created!"
 
 
-def get_build(name):
-    """Kick off a build
-    """
+def _get_build_profile(name):
+    """Return build profile with given name."""
     build_file = os.path.join(app_folder, name)
     if not os.path.exists(build_file):
-        raise ApiError("Build '%s' not found" % name, code=404)
+        raise ApiError("Build profile '%s' not found" % name, code=404)
     with open(build_file, 'r') as fd:
         build_json = json.load(fd)
+    return build_json
+
+
+def run_build(name):
+    """Kick off a build."""
+    build_json = _get_build_profile(name)
     build = Build(name, **build_json)
     def generate():
         for line in do_build(build):
@@ -185,15 +197,36 @@ def get_build(name):
     return Response(generate(), mimetype='text/plain')
 
 
-@app.route('/build/<name>', methods=['GET', 'POST'])
+def get_build_profile(name):
+    """Get the given build profile."""
+    build_json = _get_build_profile(name)
+    return jsonify(**build_json)
+
+
+@app.route('/build/<name>', methods=['POST'])
+def build(name):
+    """Run given build."""
+    return run_build(name)
+
+
+@app.route('/profile', methods=['GET'])
+def list_builds():
+    """Responsible for listing known builds."""
+    builds = []
+    for phile in listdir(app_folder):
+        fp = os.path.join(app_folder, phile)
+        if not os.path.isfile(fp):
+            continue
+        builds.append(phile)
+    return jsonify({"builds": builds})
+
+
+@app.route('/profile/<name>', methods=['GET', 'POST'])
 def build_container(name):
-    """Responsible for creating/updating builds and launching them
-    """
+    """Responsible for creating/updating builds and launching them."""
     if request.method == 'POST':
         return post_build(name)
-    if request.method == 'GET':
-        return get_build(name)
-    abort(405)
+    return get_build_profile(name)
 
 
 if __name__ == '__main__':
